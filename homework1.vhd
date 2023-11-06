9b16939e888af8812af8a11881c4b40fe4fcdbdf
Jellyfish_Yu <jellyfishyu0227@gmail.com>
2023-11-06 10:07:51 +0000

    finished output test, LED output ok

--- homework1.vhd
+++ homework1.vhd
@@ -25,45 +25,51 @@ signal LED_activating_counter: std_logic_vector(1 downto 0);
 -- activates    LED1    LED2   LED3   LED4
 -- and repeat
 signal rst : STD_LOGIC;
-signal seg_display : STD_LOGIC_VECTOR(6 downto 0);
 signal first_digit : STD_LOGIC_VECTOR(3 downto 0) := "0111";
 signal second_digit : STD_LOGIC_VECTOR(3 downto 0) := "1000";
 signal third_digit : STD_LOGIC_VECTOR(3 downto 0) := "1001";
 signal fourth_digit : STD_LOGIC_VECTOR(3 downto 0) := "1010";
+signal state : STD_LOGIC_VECTOR(1 downto 0) := "00";
 
 begin
 
 rst <= btnC;
-seg_display <= seg;
+
+OUTPUT_DETERMINE : process(state)
+begin
+    if (state = "00") 
+
+end process;
 -- VHDL code for BCD to 7-segment decoder
 -- Cathode patterns of the 7-segment LED display 
 process(LED_BCD)
 begin
     case LED_BCD is
-    when "0000" => seg_display <= "0000001"; -- "0"     
-    when "0001" => seg_display <= "1001111"; -- "1" 
-    when "0010" => seg_display <= "0010010"; -- "2" 
-    when "0011" => seg_display <= "0000110"; -- "3" 
-    when "0100" => seg_display <= "1001100"; -- "4" 
-    when "0101" => seg_display <= "0100100"; -- "5" 
-    when "0110" => seg_display <= "0100000"; -- "6" 
-    when "0111" => seg_display <= "0001111"; -- "7" 
-    when "1000" => seg_display <= "0000000"; -- "8"     
-    when "1001" => seg_display <= "0000100"; -- "9" 
-    when "1010" => seg_display <= "0000010"; -- a
-    when "1011" => seg_display <= "1100000"; -- b
-    when "1100" => seg_display <= "0110001"; -- C
-    when "1101" => seg_display <= "1000010"; -- d
-    when "1110" => seg_display <= "0110000"; -- E
-    when "1111" => seg_display <= "0111000"; -- F
+        when "0000" => seg <= "1000000"; -- "0"     
+        when "0001" => seg <= "1111001"; -- "1" 
+        when "0010" => seg <= "0100100"; -- "2" 
+        when "0011" => seg <= "0110000"; -- "3" 
+        when "0100" => seg <= "0011001"; -- "4" 
+        when "0101" => seg <= "0010010"; -- "5" 
+        when "0110" => seg <= "0000010"; -- "6" 
+        when "0111" => seg <= "1111000"; -- "7" 
+        when "1000" => seg <= "0000000"; -- "8"     
+        when "1001" => seg <= "0010000"; -- "9" 
+        when "1010" => seg <= "0100000"; -- a
+        when "1011" => seg <= "0000011"; -- b
+        when "1100" => seg <= "1000110"; -- C
+        when "1101" => seg <= "0100001"; -- d
+        when "1110" => seg <= "0000110"; -- E
+        when "1111" => seg <= "0001110"; -- F
     end case;
 end process;
 
 -- 7-segment display controller
 -- generate refresh period of 10.5ms
-process(clk,reset)
+process(clk,rst)
 begin 
-    if(reset='1') then
+    if(rst='1') then
+        displayed_number <= "0111100010010000";
         refresh_counter <= (others => '0');
     elsif(rising_edge(clk)) then
         refresh_counter <= refresh_counter + 1;
@@ -98,7 +104,7 @@ begin
     end case;
 end process;
 
-displayed_number <= first_digit & second_digit & third_digit & third_digit;
+displayed_number <= first_digit & second_digit & third_digit & fourth_digit;
 
 
 end Behavioral;
